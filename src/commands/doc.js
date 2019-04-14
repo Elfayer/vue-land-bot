4aff51dcaa90d3d9008d3974bea20948044620b4
remi.vanrenterghem01@gmail.com <remi.vanrenterghem01@gmail.com>
2019-04-15 00:08:47 +0800

    new command doc

---
+++ src/commands/doc.js
@@ -0,0 +1,101 @@
+const links = [{
+  name: 'vue',
+  aliases: ['home', 'vuejs'],
+  value: 'https://vuejs.org/'
+}, {
+  name: 'get-started',
+  aliases: ['start', 'intro', 'introduction'],
+  value: 'https://vuejs.org/v2/guide/'
+}, {
+  name: 'installation',
+  value: 'https://vuejs.org/v2/guide/installation.html'
+}, {
+  name: 'class',
+  aliases: ['classes'],
+  value: 'https://vuejs.org/v2/guide/class-and-style.html#Binding-HTML-Classes'
+}, {
+  name: 'style',
+  aliases: ['styles'],
+  value: 'https://vuejs.org/v2/guide/class-and-style.html#Binding-Inline-Styles'
+}, {
+  name: 'component',
+  aliases: ['components'],
+  value: 'https://vuejs.org/v2/guide/components.html'
+}, {
+  name: 'slot',
+  aliases: ['slots'],
+  value: 'https://vuejs.org/v2/guide/components-slots.html'
+}, {
+  name: 'prop',
+  aliases: ['props'],
+  value: 'https://vuejs.org/v2/guide/components-props.html'
+}, {
+  name: 'event',
+  aliases: ['events'],
+  value: 'https://vuejs.org/v2/guide/components-custom-events.html'
+}, {
+  name: 'registration',
+  value: 'https://vuejs.org/v2/guide/components-registration.html'
+}, {
+  name: 'transition',
+  aliases: ['transitions'],
+  value: 'https://vuejs.org/v2/guide/transitions.html'
+}, {
+  name: 'mixin',
+  aliases: ['mixins'],
+  value: 'https://vuejs.org/v2/guide/mixins.html'
+}, {
+  name: 'directive',
+  aliases: ['directives', 'custom-directive', 'custom-directives'],
+  value: 'https://vuejs.org/v2/guide/custom-directive.html'
+}, {
+  name: 'render',
+  aliases: ['render-function', 'render-functions'],
+  value: 'https://vuejs.org/v2/guide/render-function.html'
+}, {
+  name: 'lifecycle',
+  aliases: ['cycle', 'lifecycles', 'lifecycle-hooks'],
+  value: 'https://vuejs.org/v2/api/#Options-Lifecycle-Hooks'
+}, {
+  name: 'lifecycle-diagram',
+  aliases: ['cycle-diag', 'cycle-diagram', 'life-cycle-diagram'],
+  value: 'https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram'
+}, {
+  name: 'cookbook',
+  aliases: ['cook'],
+  value: 'https://vuejs.org/v2/cookbook/'
+}, {
+  name: 'style-guide',
+  aliases: ['guide'],
+  value: 'https://vuejs.org/v2/style-guide/'
+}, {
+  name: 'example',
+  aliases: ['examples'],
+  value: 'https://vuejs.org/v2/examples/'
+}, {
+  name: 'prop-event',
+  aliases: ['props-events'],
+  value: 'https://vuejs.org/images/props-events.png'
+}]
+
+module.exports = {
+  name: 'doc',
+  description: 'Match a key word with a doc link',
+  args: true,
+  isAvailable: true,
+  usage: '<keyword>',
+  execute (message, args) {
+    const value = args[0]
+    const found = links.find(
+      link => link.name === value ||
+      (link.aliases && link.aliases.some(alias => alias === value))
+    )
+
+    if (!found) {
+      const linksName = links.map(link => link.name).join(', ')
+      message.channel.send(`Documentation not found. Try: ${linksName}`)
+    } else {
+      message.channel.send(found.value)
+    }
+  }
+}
