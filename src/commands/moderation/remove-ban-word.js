4036ccbc689bb634e62584eed3f43f0e0fe79124
sustained <sustained.dissonance@gmail.com>
2019-09-01 22:19:08 +0100

    refactor: Commando-ise !remove-ban-word command.

--- src/commands/moderation/remove-ban-word.js
+++ src/commands/moderation/remove-ban-word.js
@@ -1,21 +1,36 @@
-const { banWords, saveToFile, toString } = require('../services/ban-words')
+import { Command } from "discord.js-commando"
 
-module.exports = {
-  name: 'remove-ban-word',
-  description: 'Remove a word to be used by the ban job.',
-  args: true,
-  isAvailable: true,
-  usage: '<word>',
-  execute (message, args) {
-    const word = args[0]
+import { banWords, saveToFile, toString } from "../../services/ban-words"
+
+export default class ModerationRemoveBanWordCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'remove-ban-word',
+      group: 'mod',
+      aliases: ['rbw'],
+      guildOnly: true,
+      memberName: 'mod-remove-ban-word',
+      description: 'Remove a banned word.'
+    })
+  }
+
+  /*
+    TODO: Add adequate permission check.
+  */
+  hasPermission(msg) {
+    return true
+  }
+
+  async run(msg, args) {
+    const { word } = args
     const foundIndex = banWords.findIndex(w => w.toLowerCase() === word.toLowerCase())
 
     if (foundIndex >= 0) {
       banWords.splice(foundIndex, 1)
       saveToFile()
-      message.channel.send(`Ban words: ${toString()}`)
+      msg.channel.send(`Ban words: ${toString()}`)
     } else {
-      message.channel.send(`I cannot find the word "${word}" in the ban words list.`)
+      msg.channel.send(`I cannot find the word "${word}" in the banned words list.`)
     }
   }
 }
