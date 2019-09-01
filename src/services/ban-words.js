b6fc44ad4c2726e227ffa0d161bbfede7aa40f7f
sustained <sustained.dissonance@gmail.com>
2019-09-01 21:55:41 +0100

    refactor: Update ban words service.

--- src/services/ban-words.js
+++ src/services/ban-words.js
@@ -1,9 +1,30 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import { resolve } from 'path'
+
 const FILENAME = 'ban-words.txt'
-const PATH = path.join(__dirname, FILENAME)
+const PATH = join(__dirname, "../../data/", FILENAME)
 const SEPARATOR = '\r\n'
-const banWords = []
+
+export const banWords = []
+
+export function saveToFile () {
+  fs.access(PATH, (err) => {
+    if (err) {
+      throw err
+    }
+
+    fs.writeFile(PATH, banWords.join(SEPARATOR), 'utf8', (err, data) => {
+      if (err) {
+        throw err
+      }
+      console.log(`File "${PATH}" updated.`)
+    })
+  })
+}
+
+export function toString () {
+  return banWords.reduce((acc, val) => acc ? `${acc}, \`${val}\`` : `\`${val}\``, '')
+}
 
 function _initFromFile () {
   fs.access(PATH, (err) => {
@@ -20,29 +41,4 @@ function _initFromFile () {
   })
 }
 
-function saveToFile () {
-  fs.access(PATH, (err) => {
-    if (err) {
-      throw err
-    }
-
-    fs.writeFile(PATH, banWords.join(SEPARATOR), 'utf8', (err, data) => {
-      if (err) {
-        throw err
-      }
-      console.log(`File "${PATH}" updated.`)
-    })
-  })
-}
-
-function toString () {
-  return banWords.reduce((acc, val) => acc ? `${acc}, \`${val}\`` : `\`${val}\``, '')
-}
-
 _initFromFile()
-
-module.exports = {
-  saveToFile,
-  toString,
-  banWords
-}
