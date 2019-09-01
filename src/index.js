24ec0d5c0c2d552bceb73252c4e72034eda82e7f
sustained <sustained.dissonance@gmail.com>
2019-09-01 21:49:26 +0100

    other: Error handling.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ const {
   BOT_TOKEN,
 } = process.env
 
-client.login(BOT_TOKEN)
+if ( ! BOT_TOKEN) {
+  console.error('The environmental variable BOT_TOKEN is required but was not present!')
+  process.exit()
+}
+
+try {
+  client.login(BOT_TOKEN)
+}
+catch(e) {
+  console.error('The provided BOT_TOKEN environmental variable could not be used to login!')
+  console.error(e)
+  process.exit(1)
+}
