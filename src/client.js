81c88b856c0719b9da2e0ca563c1ff46e23a0a5e
sustained <sustained.dissonance@gmail.com>
2019-09-01 21:37:15 +0100

    refactor: Set up new Commando-based client.

--- src/client.js
+++ src/client.js
@@ -1,3 +1,64 @@
-const Discord = require('discord.js')
+import { CommandoClient } from 'discord.js-commando'
 
-module.exports = new Discord.Client()
+const {
+  OWNER
+} = process.env
+
+const PATH_TYPES = join(__dirname, "types")
+const PATH_COMMANDS = join(__dirname, "commands")
+
+const client = new CommandoClient({
+  owner : OWNER
+})
+
+/*
+  Register command groups.
+
+  https://discord.js.org/#/docs/commando/master/class/CommandoRegistry?scrollTo=registerGroups
+*/
+client.registry.registerGroups([
+  {
+    id: 'dev',
+    name: 'Development'
+  },
+  {
+    id: 'docs',
+    name: 'Documentation'
+  },
+  {
+    id: 'misc',
+    name: 'Miscellaneous'
+  },
+  {
+    id: 'mod',
+    name: 'Moderation'
+  }
+])
+
+/*
+  Register default command groups, commands and argument types.
+
+  And then register our own types and commands.
+
+  https://discord.js.org/#/docs/commando/master/class/CommandoRegistry?scrollTo=registerDefaults
+  https://discord.js.org/#/docs/commando/master/class/CommandoRegistry?scrollTo=registerTypesIn
+  https://discord.js.org/#/docs/commando/master/class/CommandoRegistry?scrollTo=registerCommandsIn
+*/
+client.registry.registerDefaults()
+client.registry.registerTypesIn(PATH_TYPES)
+client.registry.registerCommandsIn(PATH_COMMANDS)
+
+/*
+  Set up some global error handling and some purely informational event handlers.
+*/
+
+client.on('warn',  console.warn)
+client.on('error', console.error)
+
+client.on('resume', () => console.info('Connection resumed.'))
+client.on('disconnect', () => console.info('Disconnected!'))
+client.on('reconnecting', () => console.info('Reconnecting...'))
+
+process.on('unhandledRejection', console.error)
+
+export default client
