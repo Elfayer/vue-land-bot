4ce3905790756f7a7889119bed1970e72093406e
sustained <sustained.dissonance@gmail.com>
2019-09-04 17:00:10 +0100

    other: Various changes.

--- src/commands/rfcs/rfc.js
+++ src/commands/rfcs/rfc.js
@@ -1,16 +1,27 @@
 import { Command } from 'discord.js-commando'
 import { RichEmbed } from 'discord.js'
-import { getRFC, RFCDoesNotExistError } from '../../services/rfcs'
+import { filterRFCs, RFCDoesNotExistError } from '../../services/rfcs'
 import { EMPTY_MESSAGE } from '../../utils/constants'
+import { tryDelete } from '../../utils/messages'
 
 module.exports = class RFCsCommand extends Command {
   constructor(client) {
     super(client, {
       args: [
         {
-          key: 'rfc',
-          type: 'string|integer',
-          prompt: 'the id of the RFC?',
+          key: 'filter',
+          type: 'string',
+          prompt: 'the filter (id, body, state, title, labels, author)?',
+          error:
+            'filter must be one of: id, body, state, title, labels, author',
+          validate(value) {
+            return ['id', 'body', 'title', 'author', 'labels'].includes(value)
+          },
+        },
+        {
+          key: 'value',
+          type: 'string',
+          prompt: 'the value?',
         },
       ],
       name: 'rfc',
@@ -35,42 +46,89 @@ module.exports = class RFCsCommand extends Command {
   }
 
   async run(msg, args) {
-    const { rfc } = args
+    let { filter, value } = args
+    let success = false
 
-    if (isNaN(rfc)) {
-      return msg.reply(
-        'Sorry, I only support fetching PRs by id/number for now.'
-      )
+    let embed = new RichEmbed()
+      .setTitle(`RFC Request`)
+      .setDescription(`Filter: ${filter}=${value}`)
+
+    try {
+      const filtered = await filterRFCs(filter, value)
+
+      if (filtered.length === 0) {
+        throw new RFCDoesNotExistError()
+      }
+
+      embed = this.buildEmbed(embed, filtered, msg.author, filter, value)
+      success = true // For finally block.
+    } catch (error) {
+      if (error instanceof RFCDoesNotExistError) {
+        embed.setDescription('RFC not found!')
+      } else {
+        console.error(error)
+        embed.setDescription('Sorry, an unspecified error occured')
+      }
+    } finally {
+      msg.channel.send(EMPTY_MESSAGE, embed).then(reply => {
+        tryDelete(msg)
+
+        // Delete the reply if the RFC was not found, or an error occured.
+        if (!success) {
+          tryDelete(reply, 7500)
+        }
+      })
+    }
+  }
+
+  buildEmbed(embed, filtered, author, filter, value) {
+    if (filtered.length === 1) {
+      return this.buildEmbedSingle(embed, filtered[0], author, filter, value)
+    } else {
+      return this.buildEmbedMultiple(embed, filtered, author, filter, value)
+    }
+  }
+
+  buildEmbedSingle(embed, rfc, author, filter, value) {
+    embed
+      .setTitle(`RFC #${rfc.number} - ${rfc.title}`)
+      .setDescription(rfc.body)
+      .setAuthor(rfc.user.login, rfc.user.avatar_url, rfc.user.html_url)
+      .setTimestamp(rfc.created_at)
+      .setURL(rfc.html_url)
+      .addField('Status', rfc.state)
+
+    if (rfc.labels.length) {
+      embed.addField('Labels', rfc.labels.map(label => label.name).join(', '))
     }
 
-    getRFC(rfc)
-      .then(rfc => {
-        const embed = new RichEmbed()
-          .setTitle(`RFC #${rfc.number} - ${rfc.title}`)
-          .setDescription(rfc.body)
-          .setAuthor(rfc.user.login, rfc.user.avatar_url, rfc.user.html_url)
-          .setTimestamp(rfc.created_at)
-          .setURL(rfc.html_url)
-          .addField('Status', rfc.state)
+    let labelsWithColours = rfc.labels.filter(label =>
+      ['core', 'vuex', 'router'].includes(label.name)
+    )
 
-        if (rfc.labels.length) {
-          embed.addField(
-            'Labels',
-            rfc.labels.map(label => label.name).join(', ')
-          )
-        }
+    if (labelsWithColours) {
+      embed.setColor(`#${labelsWithColours[0].color}`)
+    }
 
-        msg.channel.send(EMPTY_MESSAGE, { embed })
-      })
-      .catch(error => {
-        if (error instanceof RFCDoesNotExistError) {
-          return msg.reply('An RFC with that ID does not exist.')
-        }
+    if (filter !== 'id') {
+      embed.addField('Request this RFC', `\`!rfc ${filter} '${value}'\``)
+    }
 
-        console.error(error)
+    return embed
+  }
 
-        return msg.reply('Sorry, an unknown error occured.')
-      })
+  /* eslint-disable no-unused-vars */
+  buildEmbedMultiple(embed, rfcs, author, filter, value) {
+    embed.setTitle(`Found Multiple Matching RFCs`)
+
+    // TODO: Could exceed max allowed number of fields (25)?
+    for (const rfc of rfcs) {
+      embed.addField(rfc.title, `\`!rfc id ${rfc.number}\``)
+    }
+
+    embed.addField('Requested by', author)
+
+    return embed
   }
 }
 
