9cec3b9cae2464ff21c734818ac0bf447c96082c
sustained <sustained.dissonance@gmail.com>
2019-09-30 17:36:18 +0100

    misc: Export the default embed colour.

--- src/utils/embed.js
+++ src/utils/embed.js
@@ -1,6 +1,6 @@
 import { RichEmbed } from 'discord.js'
 
-const DEFAULT_EMBED_COLOUR = '#42b883'
+export const DEFAULT_EMBED_COLOUR = '#42b883'
 
 export function embedMessage(title, description) {
   return new RichEmbed()
