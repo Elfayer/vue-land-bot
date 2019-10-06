f40fb1121bcca6a20c4f7d9ed320f5d7691b9ce9
sustained <sustained.dissonance@gmail.com>
2019-10-06 19:23:41 +0100

    feat: Add OptionalKeyValuePairArgumentType.

---
+++ src/types/optional-kv-pair.js
@@ -0,0 +1,28 @@
+import { ArgumentType } from 'discord.js-commando'
+
+/**
+ * Represents an optional key-value pair. Useful for commands where you
+ * would like to allow both a shorthand form + longer disambiguation form:
+ *
+ * !rfc some title       -> ['empty', 'some title']
+ * !rfc title:some title -> ['title', 'some title']
+ */
+module.exports = class OptionalKeyValuePairArgumentType extends ArgumentType {
+  constructor(client, id = 'optional-kv-pair') {
+    super(client, id)
+  }
+
+  parse(val) {
+    let k = 'empty'
+    let v = val
+
+    if (val.includes(':')) {
+      ;[k, v] = val.split(/:(.+)/)
+    } else if (val.indexOf('#') === 0) {
+      k = 'id'
+      v = val.substr(1)
+    }
+
+    return [k.trim(), v.trim()]
+  }
+}
