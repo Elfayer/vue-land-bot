9f4dd3d3d113e2707bacfa0573997d42a80c7f28
sustained <sustained.dissonance@gmail.com>
2019-10-08 00:26:37 +0100

    other: Clean up reload command output slightly.

--- src/commands/rfcs/reload.js
+++ src/commands/rfcs/reload.js
@@ -2,6 +2,7 @@ import { Command } from 'discord.js-commando'
 import { RichEmbed } from 'discord.js'
 import { reloadCache } from '../../services/rfcs'
 import { EMPTY_MESSAGE } from '../../utils/constants'
+import { tryDelete } from '../../utils/messages'
 
 module.exports = class RFCsCommand extends Command {
   constructor(client) {
@@ -23,16 +24,23 @@ module.exports = class RFCsCommand extends Command {
 
     try {
       await reloadCache()
-      embed.setDescription(
-        'Successfully fetched the RFC PRs from Github and re-cached them to disc.'
-      )
+      embed
+        .setDescription(
+          '✅ Fetched RFC PRs from Github and re-cached them to disc.'
+        )
+        .setColor('GREEN')
     } catch (error) {
       console.error(error)
-      embed.setDescription(
-        'An error occured while fetching and recaching the RFC PRs.'
-      )
+      embed
+        .setDescription(
+          '❎ An error occured while fetching and recaching the RFC PRs.'
+        )
+        .setColor('RED')
     } finally {
-      msg.channel.send(EMPTY_MESSAGE, { embed })
+      msg.channel.send(EMPTY_MESSAGE, { embed }).then(res => {
+        tryDelete(msg)
+        tryDelete(res, 15000)
+      })
     }
   }
 }
