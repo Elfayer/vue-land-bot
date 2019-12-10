6d93ffe1343db1e9971f0516171d924903d0df38
sustained <sustained.dissonance@gmail.com>
2019-12-10 18:24:01 +0000

    refactor: Add utilities to abstract the removal of messages.

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,11 @@
 import client from '../client'
+import {
+  AUTOMATICALLY_DELETE_ERRORS,
+  AUTOMATICALLY_DELETE_INVOCATIONS,
+  DELETE_ERRORS_AFTER_MS,
+  DELETE_INVOCATIONS_AFTER_MS,
+} from './constants'
+import { CommandMessage } from 'discord.js-commando'
 
 /*
   Delete a message safely, if possible.
@@ -65,4 +72,32 @@ export function stripEmojis(str) {
     /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2694-\u2697]|\uD83E[\uDD10-\uDD5D])/g,
     ''
   )
+}
+
+/**
+ * Check if we should remove a command invocation (the message that triggered a command)
+ * and if so then remove said message.
+ *
+ * @param {CommandMessage} msg The message containing the command invocation.
+ */
+export function cleanupInvocation(msg) {
+  if (!AUTOMATICALLY_DELETE_INVOCATIONS) {
+    return
+  }
+
+  return tryDelete(msg, DELETE_INVOCATIONS_AFTER_MS)
+}
+
+/**
+ * Check if we should remove an error response (a message sent in response to a command invocation,
+ * that specifies that some kind of error occured) and if so then remove said response.
+ *
+ * @param {CommandMessage} msg The message containing the error response.
+ */
+export function cleanupErrorResponse(msg) {
+  if (!AUTOMATICALLY_DELETE_ERRORS) {
+    return
+  }
+
+  return tryDelete(msg, DELETE_ERRORS_AFTER_MS)
 }
