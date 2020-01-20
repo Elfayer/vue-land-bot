f26c73d178f5d96d1d1e09a0528706261b1a6a99
sustained <sustained.dissonance@gmail.com>
2020-01-20 00:57:04 +0000

    other: Add constants.

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * The snowflakes (IDs) of noteworthy guilds.
+ */
+export enum GUILDS {
+  PRODUCTION = '325477692906536972', // Vue Land
+  DEVELOPMENT = '617839535727968282', // Vue Land Bot Testing
+}
