4f2c239758cc1ff3e6a734afed42edccf31e8129
sustained <sustained.dissonance@gmail.com>
2020-01-20 00:45:09 +0000

    chore: Update PM2 config file.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,33 @@
 module.exports = {
   apps: [
     {
-      name: 'bot',
-      script: 'npm run serve',
+      name: 'vuebot',
+      script: 'npm run start',
       instances: 1,
       autorestart: true,
       watch: false,
       max_restarts: 10,
       restart_delay: 5000,
       max_memory_restart: '200M',
+      min_uptime: 1500,
+      ignore_watch: [
+        '.git',
+        '.github',
+        'assets',
+        'data',
+        'logs',
+        'node_modules',
+        'src',
+        'typings',
+      ],
       env: {
         NODE_ENV: 'development',
       },
       env_production: {
         NODE_ENV: 'production',
       },
-      error_file: 'logs/err.log',
-      out_file: 'logs/out.log',
+      error_file: 'logs/vuebot-err.log',
+      out_file: 'logs/vuebot-out.log',
     },
   ],
 }
