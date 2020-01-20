276a382389575c6c77e78c8b518747726ac31624
sustained <sustained.dissonance@gmail.com>
2020-01-20 00:56:11 +0000

    wip: Add RFCSchema.

---
+++ src/lib/schemas/RFCSchema.ts
@@ -0,0 +1,37 @@
+import { Client } from 'klasa'
+
+/**
+ * Extend the guild-specific schema with settings relating to
+ * the RFCService and related commands, events, tasks etc.
+ */
+Client.defaultGuildSchema.add('rfcs', rfcs => {
+  return (
+    rfcs
+      /**
+       * Is the RFCService enabled for this guild?
+       */
+      .add('enabled', 'Boolean', { default: true })
+      /**
+       * How long to wait before refetching the RFCs from Github?
+       */
+      .add('cacheTTL', 'Duration', { default: '4h' })
+      /**
+       * Who/what can use the RFCService, and/or where?
+       */
+      .add('whitelist', whitelist => {
+        return whitelist
+          .add('roles', 'Role', { array: true })
+          .add('users', 'User', { array: true })
+          .add('channel', 'TextChanel', { array: true })
+      })
+      /**
+       * Who/what can NOT use the RFCService, and/or where?
+       */
+      .add('blacklist', blacklist => {
+        return blacklist
+          .add('roles', 'Role', { array: true })
+          .add('users', 'User', { array: true })
+          .add('channel', 'TextChanel', { array: true })
+      })
+  )
+})
