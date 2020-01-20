e21b75338f950abca999bce1d2e77a83fa8bac04
sustained <sustained.dissonance@gmail.com>
2020-01-20 17:41:15 +0000

    other: Add paths.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,8 @@
+import { join, resolve } from 'path'
+
 import { getVersion } from '@utilities/miscellaneous'
+
+const BASE_PATH = resolve(__dirname, '../', '../')
 
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development'
@@ -36,10 +40,23 @@ export const PREFIX = process.env.PREFIX ?? '!'
  * Are we in development or production?
  */
 export const DEV = process.env.NODE_ENV === 'development'
+
 /**
  * The snowflakes (IDs) of noteworthy guilds.
  */
 export enum GUILDS {
   PRODUCTION = '325477692906536972', // Vue Land
   DEVELOPMENT = '617839535727968282', // Vue Land Bot Testing
+}
+
+/**
+ * Various noteworthy paths.
+ */
+export const PATHS = {
+  BASE: BASE_PATH,
+  DATA: join(BASE_PATH, 'data'),
+  JSON: join(BASE_PATH, 'data', 'providers', 'json'),
+  ASSETS: join(BASE_PATH, 'assets'),
+  IMAGES: join(BASE_PATH, 'assets', 'images'),
+  ICONS: join(BASE_PATH, 'assets', 'images', 'icons'),
 }
