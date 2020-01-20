aa23b514d6fd3cd9eb3b537066dd8b3d5e512822
sustained <sustained.dissonance@gmail.com>
2020-01-20 00:55:18 +0000

    wip: Add RFCService.

---
+++ src/services/RFCService.ts
@@ -0,0 +1,14 @@
+import Service from '@structures/Service'
+
+/**
+ * The RFCService is responsible for any and all things relating to RFCs.
+ */
+export default class RFCService extends Service {
+  run() {
+    console.log('run')
+  }
+
+  async init() {
+    console.log('init')
+  }
+}
