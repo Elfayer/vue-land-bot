b443692973deebc5ceb75f4d5f4a149d4f77057d
sustained <sustained.dissonance@gmail.com>
2020-01-21 15:32:12 +0000

    feat(github-client): Handle rate and abuse limiting.

--- src/lib/GithubClient.ts
+++ src/lib/GithubClient.ts
@@ -1,10 +1,30 @@
 import * as Octokit from '@octokit/rest'
+import OctokitPluginThrottling from '@octokit/plugin-throttling'
 
 import { NAME, VERSION } from '@libraries/constants'
 
 const { GITHUB_TOKEN } = process.env
 
-export default new Octokit({
+Octokit.plugin((OctokitPluginThrottling as unknown) as Octokit.Plugin)
+
+const client = new Octokit({
   auth: GITHUB_TOKEN,
   userAgent: `${NAME} [${VERSION}] (octokit/rest)`,
+  throttle: {
+    onRateLimit: (retryAfter: number, options) => {
+      client.log.warn(
+        `[GithubClient] Request quota exhausted for request ${options.method} ${options.url}`
+      )
+
+      console.log(`Retrying after ${retryAfter} seconds!`)
+      return true
+    },
+    onAbuseLimit: (_retryAfter: number, options) => {
+      client.log.warn(
+        `[GithubClient] Abuse detected for request ${options.method} ${options.url}`
+      )
+    },
+  },
 })
+
+export default client
