beedd659ce66fecd16f18c11baac43521bfde8dd
sustained <sustained.dissonance@gmail.com>
2020-01-21 19:21:26 +0000

    refactor(settings): Extract settings keys.

--- src/lib/schemas/RFCSchema.ts
+++ src/lib/schemas/RFCSchema.ts
@@ -1,33 +1,18 @@
 import { Client } from 'klasa'
 
+import { RFCGuildSettings, RFCClientSettings } from '@settings/rfcs'
+
 /**
  * Extend the guild-specific schema with settings relating to
  * the RFCService and related commands, events, tasks etc.
  */
 Client.defaultGuildSchema.add('rfcs', rfcs => {
-  return (
-    rfcs
-      /**
-       * Are RFC lookups enabled for this guild?
-       */
-      .add('enabled', 'Boolean', { default: true })
-  )
+  return rfcs.add(RFCGuildSettings.ENABLED, 'boolean', { default: true })
 })
 
 Client.defaultClientSchema.add('rfcs', rfcs => {
-  return (
-    rfcs
-      /**
-       * How long to wait before refetching the RFCs from Github?
-       */
-      .add('cacheTTL', 'number', { default: 1000 * 60 * 60 * 4 })
-      /**
-       * At what time were the RFCs cached?
-       */
-      .add('cachedAt', 'number')
-      /**
-       * The RFC cache.
-       */
-      .add('cache', 'any', { array: true })
-  )
+  return rfcs
+    .add(RFCClientSettings.CACHE_TTL, 'number', { default: 1000 * 60 * 60 * 4 })
+    .add(RFCClientSettings.CACHED_AT, 'number')
+    .add(RFCClientSettings.CACHE, 'any', { array: true })
 })
--- src/lib/schemas/ReleaseSchema.ts
+++ src/lib/schemas/ReleaseSchema.ts
@@ -1,40 +1,21 @@
 import { Client } from 'klasa'
 
+import { ReleaseGuildSettings } from '@settings/releases'
+
 /**
  * Extend the guild-specific schema with settings relating to releases.
  */
 Client.defaultGuildSchema.add('releases', releases => {
-  return (
-    releases
-      /**
-       * Are release announcements enabled?
-       */
-      .add('enabled', 'boolean', { default: false })
-      /**
-       * Which channel should release announcements be sent to?
-       */
-      .add('channel', 'channel')
-      /**
-       * Which role should be notified when releases are announced?
-       */
-      .add('role', 'role')
-      /**
-       * Which repositories should we announce releases for?
-       */
-      .add('repos', 'vuejs-repo', { array: true })
-      /**
-       * How often should we check for new releases, in hours.
-       *
-       * Note that if NODE_ENV is development then will become minutes, for ease of testing.
-       */
-      .add('schedule', 'number', { min: 1, default: 6, max: 24 * 7 })
-      /**
-       * When was the last release announced?
-       */
-      .add('lastRelease', 'number')
-      /**
-       * Keeps track of which version each repository is currently on.
-       */
-      .add('versions', 'any', { configurable: false })
-  )
+  return releases
+    .add(ReleaseGuildSettings.ENABLED, 'boolean', { default: false })
+    .add(ReleaseGuildSettings.CHANNEL, 'channel')
+    .add(ReleaseGuildSettings.ROLE, 'role')
+    .add(ReleaseGuildSettings.REPOS, 'vuejs-repo', { array: true })
+    .add(ReleaseGuildSettings.SCHEDULE, 'number', {
+      min: 1,
+      default: 6,
+      max: 24 * 7,
+    })
+    .add(ReleaseGuildSettings.LAST_RELEASE, 'number')
+    .add(ReleaseGuildSettings.VERSIONS, 'any', { configurable: false })
 })
