0d4b7e18aefd79de4cfd9dae81e7d54c8ef363c1
sustained <sustained.dissonance@gmail.com>
2020-01-22 14:35:44 +0000

    change(services): Services don't have a run method.

--- src/lib/structures/Service.ts
+++ src/lib/structures/Service.ts
@@ -4,11 +4,17 @@ import { Piece } from 'klasa'
  * A Service is a piece responsible for fetching/managing/caching (etc.)
  * data from an external source, for instance - a REST or GraphQL API.
  *
- * Generally, a Service corresponds to a set of commands, events (etc.).
+ * Generally, a Service corresponds to a set of commands, events, inhibitors
+ * and so on and they usually have their own schema.
  *
- * For example, the RFCService has various commands, events, tasks and so on
- * which relate to it. If the service is disabled, then those pieces are too.
+ * For example, the RFCService has various methods which commands,
+ * events, tasks and other things which relate to RFCs all make use of.
  */
 export default abstract class Service extends Piece {
-  public abstract run(message: unknown): unknown
+  /**
+   * Services don't need to implement a run method, like most other Pieces.
+   */
+  public run(message: unknown) {
+    this.log('Service called (why)???')
+  }
 }
